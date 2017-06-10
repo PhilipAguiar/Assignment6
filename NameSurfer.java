a9cc17943ae846a70434019716183408558491ae
S <s>
2017-06-10 14:51:21 -0400

    {"types":"C","runs":3}

--- NameSurfer.java
+++ NameSurfer.java
@@ -18,44 +18,47 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
  * This method has the responsibility for reading in the data base
  * and initializing the interactors at the bottom of the window.
  */
-	public void init() {
-		nameLabel = new JLabel("Name: ");
-		name = new JTextField(20);
-	
-		add(nameLabel,SOUTH);
-	    add(name,SOUTH);
-	    graph = new JButton("Graph");
-	    clear= new JButton("Clear");
-	    add(graph,SOUTH);
-	    add(clear,SOUTH);
-	    
-	    addActionListeners();
-	  name.addActionListener(this);
-	}
+	/*
+	 * File: NameSurfer.java
+	 * ---------------------
+	 * When it is finished, this program will implements the viewer for
+	 * the baby-name database described in the assignment handout.
+	 */
 
-/* Method: actionPerformed(e) */
-/**
- * This class is responsible for detecting when the buttons are
- * clicked, so you will have to define a method to respond to
- * button actions.
- */
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == name) {
-		println(name.getText());	}
-		if (e.getSource() == graph) {
-			println("graph");	}
-		if (e.getSource() == clear) {
-			println("clear");	}
-		
-	}
-	
-	
-/*instance variables*/
+	import acm.program.*;
+	import java.awt.*;
 
-private  JLabel nameLabel;
-private JTextField name;
-private JButton graph;
-private JButton clear;
-}
+	import java.awt.event.*;
+	import javax.swing.*;
 
+	public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
+		/*
+		 * File: NameSurfer.java
+		 * ---------------------
+		 * When it is finished, this program will implements the viewer for
+		 * the baby-name database described in the assignment handout.
+		 */
+
+		import acm.program.*;
+		import java.awt.*;
+
+		import java.awt.event.*;
+		import javax.swing.*;
+
+		public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
+
+			 public void init() {
+				 nameField = new JTextField(10);
+				 add(new JLabel("Name"), SOUTH);
+				 add(nameField, SOUTH);
+				 nameField.addActionListener(this);
+				 }
+				 public void actionPerformed(ActionEvent e) {
+				 if (e.getSource() == nameField) {
+				 println("Hello, " + nameField.getText());
+				 }
+				 }
+				/* Private instance variables */
+				 private JTextField nameField;
+				}
 

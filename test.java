caef57f716f7f5c1c075df03d650a41b1456c0a0
S <s>
2017-06-13 13:06:36 -0400

    {"types":"C","runs":131}

--- test.java
+++ test.java
@@ -8,7 +8,7 @@ public class test extends ConsoleProgram {
 		
 		entry = new NameSurferEntry(Sam);
 		println(entry.getName());
-		println(entry.getRank(11));
+		println(entry.getRank(10));
 		println(entry.toString());
 	}
 	
